a2bfa41af46feeeb4fbfc184e627728cff6d387a
aHnadr <omkevoi@gmail.com>
2024-12-20 14:26:51 +0300

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,83 @@
+"use strict";
+
+$(document).ready(function () {
+	/* Video Lightbox */
+	if (!!$.prototype.simpleLightboxVideo) {
+		$('.video').simpleLightboxVideo();
+	}
+
+	/*ScrollUp*/
+	if (!!$.prototype.scrollUp) {
+		$.scrollUp();
+	}
+
+	/*Responsive Navigation*/
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-trigger span").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$(this).removeClass("open");
+		} else {
+			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+			$(this).addClass("open");
+		}
+	});
+
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-mobile ul a").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$("#nav-trigger span").removeClass("open");
+		}
+	});
+
+	/* Sticky Navigation */
+	if (!!$.prototype.stickyNavbar) {
+		$('#header').stickyNavbar();
+	}
+
+	$('#content').waypoint(function (direction) {
+		if (direction === 'down') {
+			$('#header').addClass('nav-solid fadeInDown');
+		}
+		else {
+			$('#header').removeClass('nav-solid fadeInDown');
+		}
+	});
+
+});
+
+
+/* Preloader and animations */
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+$(window).load(function () { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(10).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(10).css({'overflow-y': 'visible'});
+
+	/* WOW Elements */
+	if (typeof WOW == 'function') {
+		new WOW().init();
+	}
+
+	/* Parallax Effects */
+	if (!!$.prototype.enllax) {
+		$(window).enllax();
+	}
+
+});
